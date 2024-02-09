037cd3fbab4733bd9fbb75311d4795186fae27f8
samuelffreitas <115262466+samuelffreitas@users.noreply.github.com>
2024-02-09 15:04:09 +0000

    update 2

--- samuel.c
+++ samuel.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 void BuildBoard(int Col, int Lin, char* Brd[1000][1000])
 {
     int idxCol, idxLin;
@@ -14,16 +13,27 @@ void BuildBoard(int Col, int Lin, char* Brd[1000][1000])
     }
 }
 
-void PiecesColocation(int PiecesQntty, int position, char direction)
+void ShowBoard(int Col, int Lin, char* Brd[1000][1000])
 {
+    int idxCol, idxLin;
 
+    for (idxCol = 0; idxCol < Col; idxCol++)
+    {
+        for (idxLin = 0; idxLin < Lin; idxLin++)
+        {
+            printf("%s", Brd[idxCol][idxLin]);
+        }
+    }
 }
+
 
 int main()
 {
     char* board[1000][1000];
 
     BuildBoard(5,5, board);
+
+    ShowBoard(5,5, board);
 
     return 0;
 }
