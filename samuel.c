02665a0846ea8f57c4dd5095fd4d506bcf80d1c8
samuelffreitas <115262466+samuelffreitas@users.noreply.github.com>
2024-02-09 14:23:14 +0000

    Board Build

--- samuel.c
+++ samuel.c
@@ -1,1 +1,29 @@
-dfghjkujuuuuuuuuuuuuu
+#include <stdio.h>
+
+
+void BuildBoard(int Col, int Lin, char* Brd[1000][1000])
+{
+    int idxCol, idxLin;
+
+    for (idxCol = 0; idxCol < Col; idxCol++)
+    {
+        for (idxLin = 0; idxLin < Lin; idxLin++)
+        {
+            Brd[idxCol][idxLin] = "| _ |";
+        }
+    }
+}
+
+void PiecesColocation(int PiecesQntty, int position, char direction)
+{
+
+}
+
+int main()
+{
+    char* board[1000][1000];
+
+    BuildBoard(5,5, board);
+
+    return 0;
+}
